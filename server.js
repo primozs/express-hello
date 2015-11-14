8cb426c57d337879da1ebef7dfecd77f85e61708
Primoz Susa <primoz.susa@gmail.com>
2015-11-14 20:31:54 +0100

    init step 1

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+var port = process.argv[2] || process.env.PORT || 3000;
+
+app.get('/home', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port, function () {
+  console.log('listening at ', port);
+});
+
+// node server.js
+// http://localhost:3000
+// za vsako spremembo je potrebno ponovno zagnat strežnik
