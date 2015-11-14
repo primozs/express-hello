184cc201d8f0648562510a219a607f44bc86d494
Primoz Susa <primoz.susa@gmail.com>
2015-11-14 20:39:44 +0100

    specific static file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 var port = process.argv[2] || process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.type('html');
+  res.status(200).sendFile(path.resolve(__dirname, './public/hello.html'));
 });
 
 app.listen(port, function () {
